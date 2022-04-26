7d7716ea5bdfe0acaba1a0c434eb8a9da77e24cc
Entertainment <Entertainment@Durgas-MacBook-Pro.local>
2022-04-26 21:34:32 +1000

    Custom Lists using Express Route Parameters

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,38 +11,123 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// Connect to MongoDB using Mongoose                    - 1. Mongoose Step#1
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
+// Create schema for the data you want to store         - 2. Mongoose Step#2
+const itemsSchema = { name: String };
+
+// Create Model/collection to store the data objects    - 3. Mongoose Step#3
+const Item = mongoose.model("Item", itemsSchema);
+
+// Create some sample rows i.e. documents using the above model
+const todo1 = new Item({name: "Welcome to your ToDoList 🐙 "});
+const todo2 = new Item({name: "Hit the + button to add a new item"});
+const todo3 = new Item({name: "<-- Hit this button to delete an item."});
+
+const defaultItems = [todo1, todo2, todo3];
+
+// Custom List schema & model
+const listSchema = {
+  name: String,
+  items: [itemsSchema]  
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+// Home route
 app.get("/", function(req, res) {
+  
+  // Retrieve the items from MongoDB when the homepage is requested
+  Item.find({}, function(err, foundItems){
+  
+  if (foundItems.length === 0) {
+    // Add the default items into the MongoDB backend, if foundItems is 0
+  Item.insertMany(defaultItems, function(err) {
+  if (err) {
+  console.log(err);
+  } else {
+    console.log("Default items inserted");
+  }
+  });
 
-const day = date.getDate();
+  res.redirect("/");  // Needed for first time the app is run. Redirects to root for the first time.
 
-  res.render("list", {listTitle: day, newListItems: items});
-
+ } else {
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+ }
+  });
+  
 });
+
+// Custom Lists - created on the fly using "Express Route Parameters"
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if(!err){
+      if (!foundList) { 
+        // Create a new list
+        const list = new List({name: customListName, items: defaultItems});
+        list.save();
+        res.redirect("/" + customListName);
+    } else {
+      // Show an existing list
+      res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+    }}
+  })
+});
+
+
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;          //Get the new item/todo from the post request.
+  const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  const item = new Item({name: itemName});    //Create a new Mongo document of collection type 'Item'
+  console.log("POST request ");
+  console.log(req.body);
+
+  if (listName === "Today"){
+    item.save();
     res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+app.post("/delete", function(req, res){
+  // Access the _id of the item
+  // Remove from the DB
+  // Redirect to Home / route. This will render the items from the DB
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
+  const checkedItemId = req.body.checkbox;
 
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (!err) {
+      console.log("Successfully deleted the item");
+      res.redirect("/");
+    }
+  })
+
+})
+
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
+
+// app.get("/about", function(req, res){
+//   res.render("about");
+// });
+
+
+// Application Listener
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
